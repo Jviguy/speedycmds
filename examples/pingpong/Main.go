fea35f744a4f6a49a5fd3e223fb697897731dca0
jviguy <jviguytwo2@gmail.com>
2020-10-28 14:58:01 -0400

    mid realease now working with examples

--- examples/pingpong/Main.go
+++ examples/pingpong/Main.go
@@ -5,10 +5,13 @@ import (
 	"github.com/Jviguy/GoingCommando"
 	"github.com/Jviguy/GoingCommando/command/commandmap"
 	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	dg, err := discordgo.New("Bot " + "")
+	dg, err := discordgo.New("Bot " + TOKEN)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
@@ -16,4 +19,18 @@ func main() {
 	//i might remove the making of a command.Map but atm its in for init command injection
 	handler := GoingCommando.New(dg,commandmap.New(),"!")
 	handler.GetCommandHandler().RegisterCommand("Ping",Ping{"Ping"},true)
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	_ = dg.Close()
 }
--- examples/pingpong/go.mod
+++ examples/pingpong/go.mod
@@ -3,6 +3,6 @@ module pingpong
 go 1.14
 
 require (
-	github.com/Jviguy/GoingCommando v0.0.2-0.20201028184730-ec3e83e2acfb
+	github.com/Jviguy/GoingCommando v0.0.2-0.20201028185231-19f4518389ab
 	github.com/bwmarrin/discordgo v0.22.0
 )
