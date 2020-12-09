3bfc325b583e7d077c15be9b1a6c597f591a8837
jviguy <jviguytwo2@gmail.com>
2020-12-09 14:21:26 -0500

    trying to fix tag issues

--- examples/CustomCommandHandlers/Main.go
+++ examples/CustomCommandHandlers/Main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"CustomCommandHandlers/commandhandler"
 	"fmt"
-	"github.com/Jviguy/GoingCommando/command/commandmap"
-	"github.com/bwmarrin/discordgo"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/Jviguy/GoingCommando/command/commandMap"
+	"github.com/bwmarrin/discordgo"
 )
 
-func main()  {
+func main() {
 	//my token was there for a old bot again so fuck me lmao
 	dg, err := discordgo.New("Bot " + "TOKEN HERE")
 	if err != nil {
@@ -22,10 +23,10 @@ func main()  {
 		fmt.Println("error opening connection,", err)
 		return
 	}
-	var prefixes = []string {"!","go ","bruh ","-"}
+	var prefixes = []string{"!", "go ", "bruh ", "-"}
 	//use of a custom implemented Handler that allows for multiple prefixes
-	handler := commandhandler.New(dg,commandmap.New(),prefixes)
-	handler.GetCommandHandler().RegisterCommand("ping" , Ping{"ping"},true)
+	handler := commandhandler.New(dg, commandMap.New(), prefixes)
+	handler.GetCommandHandler().RegisterCommand("ping", Ping{"ping"}, true)
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
--- examples/CustomCommandHandlers/go.mod
+++ examples/CustomCommandHandlers/go.mod
@@ -3,6 +3,10 @@ module CustomCommandHandlers
 go 1.14
 
 require (
-	github.com/Jviguy/GoingCommando v1.0.0
+	github.com/Jviguy/GoingCommando v0.0.1
+	github.com/agnivade/levenshtein v1.1.0 // indirect
 	github.com/bwmarrin/discordgo v0.22.0
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9 // indirect
+	golang.org/x/sys v0.0.0-20201207223542-d4d67f95c62d // indirect
 )
