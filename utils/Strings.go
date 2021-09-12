7f24414983d19a80fca3880b9e3d7549b902947e
Hashim <13991048+Prim69@users.noreply.github.com>
2021-09-12 00:57:55 -0400

    Use Levenshtein And Fuzzy
    Fuzzy doesn't always return a match, this PR has FindClosest use both Levenshtein and Fuzzy so the embed sends "Did you mean: `Levenshtein result` or `Fuzzy result`?", and just one of them if they're equal or if there is no fuzzy result.

--- utils/Strings.go
+++ utils/Strings.go
@@ -1,9 +1,44 @@
 package utils
 
 import (
-  "github.com/lithammer/fuzzysearch/fuzzy"
+	"github.com/agnivade/levenshtein"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
 
-func FindClosest(search string, array []string) string {
-  return fuzzy.Find(search, array)[0]
+func FindClosest(target string, list []string) string {
+	fuz := FindFuzzy(target, list)
+	lev := FindLevenshtein(target, list)
+	if  fuz == nil || *fuz == *lev {
+		return *lev
+	}
+	return "`" + *lev + "`" + " or `" + *fuz + "`?"
+}
+
+func FindFuzzy(target string, list []string) *string {
+	fuz := fuzzy.Find(target, list)
+	if len(fuz) > 0 {
+		return &fuz[0]
+	}
+	return nil
+}
+
+func FindLevenshtein(target string, list []string) *string {
+	results := []int{}
+	for _, name := range list {
+		results = append(results, levenshtein.ComputeDistance(target, name))
+	}
+	min := results[0]
+	for _, num := range results {
+		if num < min {
+			min = num
+		}
+	}
+	var index int
+	for i, val := range results {
+		if val == min {
+			index = i
+			break
+		}
+	}
+	return &list[index]
 }
